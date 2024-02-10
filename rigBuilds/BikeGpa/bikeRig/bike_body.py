289c4eb1fdb67f01a1f822de76d6e5f8e5059eb6
Daniel Ruiz Vargas <daniel@rendermotion.com>
2024-02-09 21:01:41 -0500

    Adding the bike rig
    bike rig

---
+++ rigBuilds/BikeGpa/bikeRig/bike_body.py
@@ -0,0 +1,14 @@
+from RMPY.rig import rigWorld
+from RMPY.rig import rigSingleJoint
+
+rig_world = rigWorld.RigWorld()
+cog = rigSingleJoint.RigSingleJoint()
+cog.create_point_base('C_COG_reference_pnt')
+handle_bar_rig = rigSingleJoint.RigSingleJoint()
+handle_bar_rig.create_point_base('C_handleBar_reference_pnt')
+seat_rig = rigSingleJoint.RigSingleJoint()
+seat_rig.create_point_base('C_seat_reference_pnt')
+handle_bar_rig.set_parent(cog)
+seat_rig.set_parent(cog)
+cog.set_parent(rig_world)
+
