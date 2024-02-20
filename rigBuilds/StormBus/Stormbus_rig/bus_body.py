c738455ffbdc55fbbdbb80432a65973d7748b62e
Seldon552 <m.bouquot@rubika-edu.ca>
2024-02-20 17:57:41 -0500

    Update bus_body.py

--- rigBuilds/StormBus/Stormbus_rig/bus_body.py
+++ rigBuilds/StormBus/Stormbus_rig/bus_body.py
@@ -4,3 +4,9 @@ import pymel.core as pm
 
 def build() :
      #creating main joints of bus and wheels
+    rig_world = rigWorld.RigWorld()
+    cog = rigSingleJoint.RigSingleJoint()
+    cog.create_point_base('C_COG_reference_pnt', centered=True)
+
+if __name__ == '__main__':
+    build()
