c13427179dd6f464e09afc3a27402f219180e2a8
Seldon552 <m.bouquot@rubika-edu.ca>
2024-03-22 18:11:11 -0400

    child

---
+++ rigBuilds/ChildA/rig_build.py
@@ -0,0 +1,14 @@
+from bgb_short.pipeline import environment
+from bgb_short.pipeline.mgear import io
+import pymel.core as pm
+import importlib
+from RMPY.rig import rigFacial
+from RMPY.rig import rigBlendShapeControls
+
+
+def custom_rig():
+    pm.parent('MommyA_grp', 'rig')
+
+
+def custom_finalize():
+    pass
